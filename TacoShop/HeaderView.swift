533d86a33410f91a345743fead3674fa543d0416
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-02-13 13:40:42 -0800

    Create a headerView class

---
+++ TacoShop/HeaderView.swift
@@ -0,0 +1,11 @@
+//
+//  HeaderView.swift
+//  TacoShop
+//
+//  Created by spencerdezartsmith on 2/13/17.
+//  Copyright © 2017 spencerdezartsmith. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView, DropShadow {}
