ab33e69f8650af714091dfe3d2c672723fea7e04
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-02-14 11:08:27 -0800

    Create an extension for UICollectionViewCell

--- TacoShop/UICollectionView+Ext.swift
+++ TacoShop/UICollectionView+Ext.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2017 spencerdezartsmith. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+    func register<T: UICollectionViewCell>(_: T.Type) where T: ReuseableView, T: NibLoadableView {
+        
+        let nib = UINib(nibName: T.nibName, bundle: nil)
+        register(nib, forCellWithReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueReuseableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReuseableView {
+        
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath as IndexPath) as? T else { fatalError("Could not dequeue cell with identifier \(T.reuseIdentifier)")}
+        
+        return cell
+    }
+}
+
+extension UICollectionViewCell: ReuseableView {}
