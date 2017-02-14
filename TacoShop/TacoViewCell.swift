edafbc986999432738d71fdd163b89858922154d
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-02-13 18:07:26 -0800

    Begin set up for the taco view cell

---
+++ TacoShop/TacoViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TacoViewCell.swift
+//  TacoShop
+//
+//  Created by spencerdezartsmith on 2/13/17.
+//  Copyright © 2017 spencerdezartsmith. All rights reserved.
+//
+
+import UIKit
+
+class TacoViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
